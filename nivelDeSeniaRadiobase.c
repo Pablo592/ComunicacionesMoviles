027622ba3c142420f0de00208c7300133f2ad1d0
Pablo592 <pgaido524@alumnos.iua.edu.ar>
2023-06-07 20:12:08 -0300

    calculo actualizado

--- nivelDeSeniaRadiobase.c
+++ nivelDeSeniaRadiobase.c
@@ -8,16 +8,34 @@ double HM = 1.5; // Altura de la antena del móvil en m.
 double HB = 30; // Altura efectiva de la estaciòn base en metros
 double CM = -12; // Factor de corrección (Para entornos sub-urbanos con pequeños edificios)
 double D = 3.10901; //Distancia D en Km
+double PTX = 30;    //Potencia de la antena
+double GTX = 14;      // Ganancia de la antena en DB. Fuente
+double GRX = 2;      // Ganancia del celular
+double atenuacionConectores = 1;
+double conectorCelular = 1;
+
+
+
+//Es decir, la potencia recibida por el celular Prx, los -101 dBm, es igual a la potencia transmitida por la 
+//antena de la torre mas la ganancia de la antena menos la atenuacion del medio menos 1 dB de atenuacion de 
+//los conectores mas la ganancia de la antena del celular que pueden ser 2dB menos 1 dB de atenuacio del conector del celular.
+
 
 
 double l();
 double a();
+double prx();
 
 int main() {
 
-    printf("Los decibeles que me llegan son: %f \n", l());
+    printf("Los decibeles que me llegan son: %f \n", prx());
 
     return 0;
+}
+
+//Prx = Ptx + Gtx - 144 dB - 1 dB + Grx - 1 dB
+double prx(){
+return PTX + GTX - l() - atenuacionConectores + GRX - conectorCelular;
 }
 
 //L( dB ) = 46,3 + 33,9 log( f c ) − 13,82 log( hb ) − a( hm ) + ( 44,9 − 6,55 log( hb )) log(d) + Cm
